e301b283ae4eb002a56d7a9f4e8d1f6f7f5f6084
horden316 <horden316@gmail.com>
2022-12-11 21:16:22 +0800

    feat: 新增財務結構、償債能力比率

--- income_draw.R
+++ income_draw.R
@@ -2,7 +2,9 @@ income_draw <- function() {
 
   ################################################################################
   # plot title, color
-  ratio <- c("純益率 (%)")
+  #ratio <- c("負債佔資產比率","純益率%","長期資金佔固定資產比率","流動比率","速動比率")
+  ratio <- c("負債佔資產比率","長期資金佔固定資產比率","流動比率","速動比率",
+             "利息保障倍數")
   color <- c("orange", "blue")
   #
   ################################################################################
@@ -19,12 +21,50 @@ income_draw <- function() {
   ################################################################################
   #
   # ratio calculate
+  
+  #負債佔資產比率
+  Ratio <- balance_dta[which(mAcc_balances == "　負債總額"), , ] / (
+    balance_dta[which(mAcc_balances == "　資產總額"), , ] )
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[1, , 1] <- unlist(Ratio[1, ])
+  dtax[1, , 2] <- unlist(Ratio[2, ])
+  
+  #長期資金佔固定資產比率
+  Ratio <- (balance_dta[which(mAcc_balances == "　權益總額"), , ]+balance_dta[which(mAcc_balances == "　　非流動負債合計"), , ]) /
+    balance_dta[which(mAcc_balances == "　　非流動資產合計"), , ]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[2, , 1] <- unlist(Ratio[1, ])
+  dtax[2, , 2] <- unlist(Ratio[2, ])
+  #
+  #
+  #流動比率
+  Ratio <- (balance_dta[which(mAcc_balances == "　　流動資產合計"), , ]) /
+    balance_dta[which(mAcc_balances == "　　流動負債合計"), , ]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[3, , 1] <- unlist(Ratio[1, ])
+  dtax[3, , 2] <- unlist(Ratio[2, ])
+
+  #速動比率
+  Ratio <- (balance_dta[which(mAcc_balances == "　　流動資產合計"), , ]-balance_dta[which(mAcc_balances == "　　　存貨"), , ]-balance_dta[which(mAcc_balances == "　　　預付款項"), , ]) /
+    balance_dta[which(mAcc_balances == "　　流動負債合計"), , ]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[4, , 1] <- unlist(Ratio[1, ])
+  dtax[4, , 2] <- unlist(Ratio[2, ])
+
+  #利息保障倍數
+  Ratio <- (income_dta[which(mAcc_income == "稅前淨利（淨損）"), , ]+income_dta[which(mAcc_income == "所得稅費用（利益）合計"), , ]) /
+    balance_dta[which(mAcc_balances == "　　流動負債合計"), , ]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[5, , 1] <- unlist(Ratio[1, ])
+  dtax[5, , 2] <- unlist(Ratio[2, ])
+
+
   # 純益率=本期淨利（淨損）/　銷貨收入淨額
-  Net_profit_margin <- income_dta[which(mAcc_income=="本期淨利（淨損）"), , ] / 
-    income_dta[which(mAcc_income=="　銷貨收入淨額"), , ]
-  Net_profit_margin <- as.data.frame(t(Net_profit_margin))
-  dtax[, , 1] <- unlist(Net_profit_margin[1, ])
-  dtax[, , 2] <- unlist(Net_profit_margin[2, ])
+  # Net_profit_margin <- income_dta[which(mAcc_income=="本期淨利（淨損）"), , ] /
+  #   income_dta[which(mAcc_income=="　銷貨收入淨額"), , ]
+  # Net_profit_margin <- as.data.frame(t(Net_profit_margin))
+  # dtax[, , 1] <- unlist(Net_profit_margin[1, ])
+  # dtax[, , 2] <- unlist(Net_profit_margin[2, ])
   #
   ################################################################################
   #
