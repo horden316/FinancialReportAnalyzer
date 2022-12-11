9136b7bbeeb8606553dafec982e13c64be9a0829
curtis <curtisyan90@gmail.com>
2022-12-11 22:20:13 +0800

    Update income_draw
    add some ratio

--- income_draw.R
+++ income_draw.R
@@ -8,23 +8,57 @@ income_draw <- function() {
   ################################################################################
   #
   # define array
-  setwd(root)
-  setwd(file.path(stock[1], "csv/comprehensiveincome", fsep = ""))
   rnum <- length(ratio) # Ratio - row
   cnum <- length(dir()) # Year - column
   dnum <- length(stock) # Stock - dimension
   dtax <- array(numeric(), c(rnum, cnum, dnum)) # 3D array
-  dimnames(dtax) <- list(ratio, substr(dir(), 6, 9), stock) # named array
+  dimnames(dtax) <- list(ratio, year, stock) # named array
   #
   ################################################################################
   #
   # ratio calculate
+
+  #應收款項週轉率=銷貨淨額÷各期平均應收款項餘額
+  for (y in 1:cnum) {
+    
+  }
+  Ratio <- income_dta[which(mAcc_income == "　銷貨收入淨額"), y, ] /
+    (((cash_dta[which(mAcc_cash == "　　　　應收帳款（增加）減少"), y, ] +
+         cash_dta[which(mAcc_cash == "　　　　應收帳款－關係人（增加）減少"), y, ] +
+         cash_dta[which(mAcc_cash == "　　　　其他應收款（增加）減少"), y, ] +
+         cash_dta[which(mAcc_cash == "　　　　其他應收款－關係人（增加）減少"), y, ]) +
+        (cash_dta[which(mAcc_cash == "　　　　應收帳款（增加）減少"), y-1, ] +
+           cash_dta[which(mAcc_cash == "　　　　應收帳款－關係人（增加）減少"), y-1, ] +
+           cash_dta[which(mAcc_cash == "　　　　其他應收款（增加）減少"), y-1, ] +
+           cash_dta[which(mAcc_cash == "　　　　其他應收款－關係人（增加）減少"), y-1, ])
+    ) / 2)
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[1, , 1] <- unlist(Ratio[1, ])
+  dtax[1, , 2] <- unlist(Ratio[2, ])
+  #
+  #平均收現日數=365÷應收款項週轉率
+  #
+  #存貨週轉率=銷貨成本÷平均存貨額
+  #
+  #平均售貨日數=365÷存貨週轉率
+  #
+  #固定資產週轉率=銷貨淨額÷固定資產淨額
+  #
+  #總資產週轉率=銷貨淨額÷資產總額
+  #
+  #資產報酬率=[稅後損益+利息費用(1-稅率)]÷平均資產總額
+  #
+  #股東權益報酬率=稅後損益÷平均股東權益淨額
+  #
   # 純益率=本期淨利（淨損）/　銷貨收入淨額
-  Net_profit_margin <- income_dta[which(mAcc_income=="本期淨利（淨損）"), , ] / 
+  Ratio <- income_dta[which(mAcc_income=="本期淨利（淨損）"), , ] / 
     income_dta[which(mAcc_income=="　銷貨收入淨額"), , ]
-  Net_profit_margin <- as.data.frame(t(Net_profit_margin))
-  dtax[, , 1] <- unlist(Net_profit_margin[1, ])
-  dtax[, , 2] <- unlist(Net_profit_margin[2, ])
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[2, , 1] <- unlist(Ratio[1, ])
+  dtax[2, , 2] <- unlist(Ratio[2, ])
+  #
+  #每股盈餘=(稅後淨利-特別股股利)÷加權平均已發行股數
+  #
   #
   ################################################################################
   #
@@ -32,18 +66,18 @@ income_draw <- function() {
   for (r in 1:rnum) {
     Yrange <- c(min(dtax[r, , ]) / 1.1, max(dtax[r, , ]) * 1.1) # Y
     for (d in 1:dnum) {
-      plot(unlist(dtax[, , d]),
+      plot(unlist(dtax[r, , d]),
         type = "l", col = paste(color[d]), lwd = 2, main = paste(ratio[r]),
         ylim = Yrange, ylab = "", xlab = "年", xaxt = "n"
       )
-      points(unlist(dtax[, , d]), pch = 16, cex = 1.25, col = paste(color[d]))
+      points(unlist(dtax[r, , d]), pch = 16, cex = 1.25, col = paste(color[d]))
       if (d != dnum) {
         par(new = T)
       }
     }
     axis(
-      1, 1:length(colnames(income_dta)),
-      format(as.Date(colnames(income_dta), format = "%Y"), "%Y")
+      1, 1:length(year),
+      format(as.Date(as.character(year), format = "%Y"), "%Y")
     ) # X
   }
   
