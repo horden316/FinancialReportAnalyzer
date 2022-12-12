6f52ed5b6b4a3d9387450cf6e0ad9c5e721b9779
curtis <curtisyan90@gmail.com>
2022-12-13 00:09:03 +0800

    Add lot of ratio

--- draw.R
+++ draw.R
@@ -2,12 +2,12 @@ draw <- function() {
 
   ################################################################################
   # plot title, color
-  # ratio <- c("負債佔資產比率", "長期資金佔固定資產比率", "流動比率", "速動比率", 
-  #            "利息保障倍數", "應收帳款週轉率", "平均收現日數", "存貨週轉率", "平均售貨日數",
-  #            "固定資產週轉率", "總資產週轉率", "資產報酬率", "股東權益報酬率", "純益率", "每股盈餘", 
-  #            "現金流量比率", "現金再投資比率")
-  ratio <- c("負債佔資產比率","長期資金佔固定資產比率","流動比率","速動比率",
-             "利息保障倍數", "應收帳款週轉率")
+  ratio <- c("負債佔資產比率", "長期資金佔固定資產比率", "流動比率", "速動比率",
+             "利息保障倍數", "應收帳款週轉率", "平均收現日數", "存貨週轉率", "平均售貨日數",
+             "固定資產週轉率", "總資產週轉率", "資產報酬率", "股東權益報酬率", "純益率", "每股盈餘",
+             "現金流量比率", "現金再投資比率")
+  # ratio <- c("負債佔資產比率","長期資金佔固定資產比率","流動比率","速動比率",
+  #            "利息保障倍數", "應收帳款週轉率")
   
   color <- c("orange", "blue")
   #
@@ -73,33 +73,84 @@ draw <- function() {
       balance_dta[which(mAcc_balances == "　　　應收帳款－關係人淨額"), dy, ]
     ) / 2
     )
-
   Ratio <- as.data.frame(t(Ratio))
   dtax[6, , 1] <- unlist(Ratio[1, ])
   dtax[6, , 2] <- unlist(Ratio[2, ])
-
-
-  # # 純益率=本期淨利（淨損）/　銷貨收入淨額
-  # Net_profit_margin <- income_dta[which(mAcc_income=="本期淨利（淨損）"), , ] /
-  #   income_dta[which(mAcc_income=="　銷貨收入淨額"), , ]
-  # Net_profit_margin <- as.data.frame(t(Net_profit_margin))
-  # dtax[14, , 1] <- unlist(Net_profit_margin[1, ])
-  # dtax[14, , 2] <- unlist(Net_profit_margin[2, ])
+  
+  #平均收現日數
+  dtax[7, , ] <- 365 / dtax[6, , ]
+  
+  #存貨週轉率
+  Ratio <- income_dta[which(mAcc_income == "　銷貨成本"), ry, ] /
+    ((balance_dta[which(mAcc_balances == "　　　存貨"), ry,] + balance_dta[which(mAcc_balances == "　　　存貨"), dy, ]) / 2)
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[8, , 1] <- unlist(Ratio[1,])
+  dtax[8, , 2] <- unlist(Ratio[2,])
+  
+  #平均售貨日數
+  dtax[9, , ] <- 365 / dtax[8, , ]
+  
+  #固定資產週轉率
+  Ratio <- income_dta[which(mAcc_income == "　銷貨收入淨額"), ry, ] /
+    balance_dta[which(mAcc_balances == "　　非流動資產合計"), ry,]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[10, , 1] <- unlist(Ratio[1,])
+  dtax[10, , 2] <- unlist(Ratio[2,])
+  
+  #總資產週轉率
+  Ratio <- income_dta[which(mAcc_income == "　銷貨收入淨額"), ry, ] /
+    balance_dta[which(mAcc_balances == "　資產總額"), ry,]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[11, , 1] <- unlist(Ratio[1,])
+  dtax[11, , 2] <- unlist(Ratio[2,])
+  
+  #資產報酬率
+  Ratio <- (income_dta[which(mAcc_income == "本期淨利（淨損）"), ry,] + cash_dta[which(mAcc_cash == "　　　利息費用"), ry, ] *
+    (1 - (income_dta[which(mAcc_income == "所得稅費用（利益）合計"), ry,] / income_dta[which(mAcc_income == "稅前淨利（淨損）"), ry,]))) /
+    ((balance_dta[which(mAcc_balances == "　資產總額"), ry, ] + balance_dta[which(mAcc_balances == "　資產總額"), dy, ]) /
+       2)
+  # Ratio <- income_dta[which(mAcc_income == "本期淨利（淨損）"), ry,]/
+  #   ((balance_dta[which(mAcc_balances == "　資產總額"), ry, ] + balance_dta[which(mAcc_balances == "　資產總額"), dy, ]) /
+  #      2)
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[12, , 1] <- unlist(Ratio[1,])
+  dtax[12, , 2] <- unlist(Ratio[2,])
+  
+  #股東權益報酬率
+  Ratio <- income_dta[which(mAcc_income == "本期淨利（淨損）"), ry, ] /
+    ((balance_dta[which(mAcc_balances == "　權益總額"), ry,] + balance_dta[which(mAcc_balances == "　權益總額"), dy,]) /
+       2)
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[13, , 1] <- unlist(Ratio[1,])
+  dtax[13, , 2] <- unlist(Ratio[2,])
+  
+  # 純益率
+  Ratio <- income_dta[which(mAcc_income == "本期淨利（淨損）"), ry,] /
+    income_dta[which(mAcc_income == "　銷貨收入淨額"), ry,]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[14, , 1] <- unlist(Ratio[1,])
+  dtax[14, , 2] <- unlist(Ratio[2,])
+  
+  #每股盈餘
+  Ratio <- income_dta[which(mAcc_income == "　基本每股盈餘"), ry,]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[15, , 1] <- unlist(Ratio[1,])
+  dtax[15, , 2] <- unlist(Ratio[2,])
   
   
-  # #現金流量比率
-  # Ratio <- (cash_dta[which(mAcc_cash == "營業活動之淨現金流入（流出）"), , ]) /
-  #   balance_dta[which(mAcc_balances == "　　流動負債合計"), , ]
-  # Ratio <- as.data.frame(t(Ratio))
-  # dtax[16, , 1] <- unlist(Ratio[1, ])
-  # dtax[16, , 2] <- unlist(Ratio[2, ])
-  # 
-  # #現金再投資比率
-  # Ratio <- (cash_dta[which(mAcc_cash == "營業活動之淨現金流入（流出）"), , ]-cash_dta[which(mAcc_cash == "　發放現金股利"), , ]) /
-  #   (balance_dta[which(mAcc_balances == "　　非流動資產合計"), , ]+balance_dta[which(mAcc_balances == "　　流動資產合計"), , ]-balance_dta[which(mAcc_balances == "　　流動負債合計"), , ])
-  # Ratio <- as.data.frame(t(Ratio))
-  # dtax[17, , 1] <- unlist(Ratio[1, ])
-  # dtax[17, , 2] <- unlist(Ratio[2, ])
+  #現金流量比率
+  Ratio <- (cash_dta[which(mAcc_cash == "營業活動之淨現金流入（流出）"), ry, ]) /
+    balance_dta[which(mAcc_balances == "　　流動負債合計"), ry, ]
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[16, , 1] <- unlist(Ratio[1, ])
+  dtax[16, , 2] <- unlist(Ratio[2, ])
+
+  #現金再投資比率
+  Ratio <- (cash_dta[which(mAcc_cash == "營業活動之淨現金流入（流出）"), ry, ]-cash_dta[which(mAcc_cash == "　發放現金股利"), ry, ]) /
+    (balance_dta[which(mAcc_balances == "　　非流動資產合計"), ry, ]+balance_dta[which(mAcc_balances == "　　流動資產合計"), ry, ]-balance_dta[which(mAcc_balances == "　　流動負債合計"), ry, ])
+  Ratio <- as.data.frame(t(Ratio))
+  dtax[17, , 1] <- unlist(Ratio[1, ])
+  dtax[17, , 2] <- unlist(Ratio[2, ])
   
   
   #
@@ -109,9 +160,9 @@ draw <- function() {
   print(dtax)
   for (r in 1:rnum) {
     Yrange <- c(min(dtax[r, , ]) / 1.1, max(dtax[r, , ]) * 1.1) # Y
-    print(paste("r: ",r))
+    #print(paste("r: ",r))
     for (d in 1:dnum) {
-      print(paste("d: ",d))
+      #print(paste("d: ",d))
       plot(unlist(dtax[r, , d]),
         type = "l", col = paste(color[d]), lwd = 2, main = paste(ratio[r]),
         ylim = Yrange, ylab = "", xlab = "年", xaxt = "n"
